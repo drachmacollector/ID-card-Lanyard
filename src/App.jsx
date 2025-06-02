0d4ae31c9c1d330f745d8a55320d39d2c3cbd011
Nakul <agentvasco69@gmail.com>
2025-06-03 00:25:47 +0530

    Created lanyard file and added favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Lanyard from './Lanyard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
+    </div>
     </>
-  )
+  );
 }
 
 export default App
